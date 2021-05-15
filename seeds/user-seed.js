d9ca8ba9b01fcf05e65cad7daf225ee7ead155be
Antonio Barrera <Barrera.antonio@gmail.com>
2021-05-15 15:10:54 -0700

    trying to test

--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -2,24 +2,28 @@ const {User} = require('../models');
 
 const userData = [
     {
+        id: 1,
         username: "Test1",
         github: "testhub",
         email: "Test@test.com",
         password: "password1"
     },
     {
+        id: 2,
         username: "Test2",
         github: "test2hub",
         email: "Test2@test.com",
         password: "password2"
     },
-    {
+    {   
+        id: 3,
         username: "Test3",
         github: "test3hub",
         email: "Test3@test.com",
         password: "password3"
     },
     {
+        id:4,
         username: "Test4",
         github: "test4hub",
         email: "Test4@test.com",
